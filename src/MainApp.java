753113a94b4e7cd79626cc9d4107f186288a349b
Сапрон2056 <sapron4ik1204@vk.com>
2021-11-22 16:41:38 +0300

    test commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,18 @@
 public class MainApp {
     public static void main(String[] args) {
-
+        int a = 10;
+        int b = 20;
     }
 
+    public boolean check(int a, int b) {
+        int s = a + b;
+        return (10 <= s) && (s <= 20);
+    }
+
+    static void PositiveOrNegative(int num) {
+        String word = "Положительное";
+        if (num < 0) word = "Отрицательное";
+
+        System.out.println(word);
+    }
 }
