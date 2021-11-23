cc90625c840b34218ca8f561e73a77c745abcfea
Сапрон2056 <sapron4ik1204@vk.com>
2021-11-23 09:14:42 +0300

    Доделал

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,22 +2,41 @@ public class MainApp {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        //1 задание
+        System.out.println(check(6, 8));
+        System.out.println(check(3, 1));
+        //2 задание
+        positiveOrNegative(7);
+        positiveOrNegative(-5);
+        //3 задание
+        System.out.println(three(6));
+        System.out.println(three(-7));
+        //4 задание
+        printSmile("Smile",3);
     }
 
-    public boolean check(int a, int b) {
+    public static boolean check(int a, int b) {
         int s = a + b;
         return (10 <= s) && (s <= 20);
     }
 
-    static void PositiveOrNegative(int num) {
-        String word = "Положительное";
-        if (num < 0) word = "Отрицательное";
-
-        System.out.println(word);
+    public static void positiveOrNegative(int n) {
+        if (n >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
     }
 
-    static public void test()
-    {
-        System.out.println("test");
+    static boolean three(int a) {
+        return a > 0;
+
+
+    }
+
+    public static void printSmile(String Smile, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println("Smile");
+        }
     }
 }
